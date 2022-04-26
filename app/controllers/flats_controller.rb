5a23d4bb49ac5919aad57ee29034bd11db901d47
Alex Cattell <abcattell91@gmail.com>
2022-04-26 20:45:23 +0100

    Generated controller and populated actions

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,47 @@
+class FlatsController < ApplicationController
+  # frozen_string_literal: true
+  before_action :find_flat, only: %i[show edit update destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def show; end
+
+  def update
+    @flat.update(flat_params)
+
+    redirect_to flat_path
+  end
+
+  def destroy
+    @flat.destroy
+
+    redirect_to flat_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'cleaning up database'
+Flat.destroy_all
+puts 'database is clean'
+
+puts 'Creating flats...'
+10.times do
+  flat = Flat.create(
+    name: ['Light & Spacious Garden Flat', 'Stylish House Close to River Thames', 'St Pancras Clock Tower Guest Suite'].sample,
+    address: Faker::Address.street_address,
+    description: "A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory",
+    price_per_night: rand(50..999),
+    number_of_guests: rand(1..10)
+  )
+
+  puts "flat #{flat.id} is created"
+end
+puts 'done'
